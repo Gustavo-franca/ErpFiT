6ba9cd3c66055badc0dabc1eb8cd1b4de60922b9
Gustavo <gustavocandidofranca@gmail.com>
2021-05-13 12:10:07 -0300

    chore: retira dependencias nao usadas

--- src/infrastructure/repositories/country/CountryRepository.java
+++ src/infrastructure/repositories/country/CountryRepository.java
@@ -6,13 +6,9 @@
 package infrastructure.repositories.country;
 import Domain.interfaces.ICountry;
 import infrastructure.database.ConnectionFactory;
-import infrastructure.repositories.IRepository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
