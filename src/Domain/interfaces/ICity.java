9cd3aa31ea85505aa075cc198e09101deec97acf
Gustavo <gustavocandidofranca@gmail.com>
2021-05-13 12:02:54 -0300

    chore: adiciona interfaces de endereços

---
+++ src/Domain/interfaces/ICity.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain.interfaces;
+
+/**
+ *
+ * @author gustavo
+ */
+public interface ICity extends IState {
+    public String city();
+    
+}
