979b8e88fd7ab83f8ce93eca83cbc3d416f4ebc4
Gustavo <gustavocandidofranca@gmail.com>
2021-05-17 22:01:04 -0300

    chore: adiciona findIdByDesc e findByDesc a interface de repository

--- src/infrastructure/repositories/IRepository.java
+++ src/infrastructure/repositories/IRepository.java
@@ -2,4 +2,6 @@ package infrastructure.repositories;
 
 public interface IRepository<T> {
     public void register(T t);
+    int findIdByDesc(String description);
+    T findByDesc(String description);
 }
